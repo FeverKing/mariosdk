9a9e907cdf603ffd02f79820275d843bd0b33e37
FeverKing <34575868+FeverKing@users.noreply.github.com>
2024-09-18 20:18:32 +0800

    modify: .gitignore

--- sdk/client_test.go
+++ sdk/client_test.go
@@ -42,7 +42,7 @@ func TestDefaultClient_GetUserInfoForCompetition(t *testing.T) {
 	client := sdkclient.NewClient()
 	client.Config.SetAccessKey("Z9r1DyumUwpUYaNg")
 	client.Config.SetSecretKey("qoTYWJM88pBb8H-2qDTI2ayqswFkKYQj")
-	client.Config.AddEndpoint("https://mario-syclover.geesec.com/api")
+	client.Config.AddEndpoint("https://mario.test.geesec.com/api")
 	err := client.Auth()
 	if err != nil {
 		t.Errorf("Auth() failed: %v", err)
